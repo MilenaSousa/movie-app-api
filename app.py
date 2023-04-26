4d5976372fbaad5283da6ba7a320e634004a1bce
Milena Sousa <milenadesousa890@gmail.com>
2023-04-26 18:33:28 -0400

    v1.0

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+with open("movies.json") as dados:
+    movies = json.load(dados)
+
+
+# Retorna todos os livros
+@app.route('/movies', methods = ['GET'])
+def get_movies():
+    return movies
+
+# Retorna apenas um livro pelo ID
+@app.route('/movies/<int:id>', methods= ['GET'])
+def get_movie_by_id(id):
+    for movie in movies:
+        if movie.get('id') == id:
+            return jsonify(movie)
+
+app.run(host='localhost', port=5000)
